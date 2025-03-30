c70aba3bcfc3c6de210cd05b10ed045a32fd0056
Mark Gonzales <markg6528@outlook.com>
2025-03-30 17:29:50 +1000

    AWS Instance Public IP adress update #2

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.25.217.115', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.25.217.115:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
